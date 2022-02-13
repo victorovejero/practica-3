83b8b99852dc304dd62094003092da2e98adcc55
victor.ovejero <victoroc162@gmail.com>
2022-02-13 12:18:43 +0100

    msad

--- untitled folder/SVM.R
+++
@@ -1,6 +0,0 @@
-data.raw <- read.csv("qsar_fish_toxicity.csv",sep=";",
-                     stringsAsFactors = TRUE,
-                     check.names = TRUE, na.strings = c("", "NA"), header=TRUE)
-library(e1071) # SVM
-
-
